a8983c6d1204b62db808c9f3dc9058ce7cf01ebb
kaanspgl <kaansapoglu@gmail.com>
2025-02-16 06:43:46 +0300

    Made the Ball follow player

--- My project/Assets/Scripts/BallController.cs
+++ My project/Assets/Scripts/BallController.cs
@@ -3,30 +3,29 @@ using UnityEngine.Events;
 
 public class BallController : MonoBehaviour
 {
-
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform BallAnchor;
     [SerializeField] private InputManager inputManager;
 
     private bool isBallLaunched;
-
     private Rigidbody ballRB;
+    
 
     void Start()
     {
-
         ballRB = GetComponent<Rigidbody>();
-
         inputManager.OnSpacePressed.AddListener(LaunchBall);
-
+        transform.parent = BallAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     private void LaunchBall()
     {
-
         if (isBallLaunched) return;
-        
         isBallLaunched = true;
-        ballRB.AddForce(transform.forward*force, ForceMode.Impulse);
-
+        transform.parent = null;
+        ballRB.isKinematic = false;
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
