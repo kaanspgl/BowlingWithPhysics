127be1b2e48f1b9fe1e9e6fc154a0c86e9547a5d
kaanspgl <kaansapoglu@gmail.com>
2025-02-16 03:39:12 +0300

    Created project, added pins, bowling ball, and ability to shoot the ball

---
+++ My project/Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private bool isBallLaunched;
+
+    private Rigidbody ballRB;
+
+    void Start()
+    {
+
+        ballRB = GetComponent<Rigidbody>();
+
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+
+    }
+
+    private void LaunchBall()
+    {
+
+        if (isBallLaunched) return;
+        
+        isBallLaunched = true;
+        ballRB.AddForce(transform.forward*force, ForceMode.Impulse);
+
+    }
+}
